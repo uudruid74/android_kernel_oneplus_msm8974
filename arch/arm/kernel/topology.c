f91e98fa287c05ade21059c9b9f739b5707395b5
Vincent Guittot <vincent.guittot@linaro.org>
2012-07-10 14:11:11 +0100

    ARM: 7462/1: topology: factorize the update of sibling masks
    This factorization has also been proposed in another patch that has not been
    merged yet:
    http://lists.infradead.org/pipermail/linux-arm-kernel/2012-January/080873.html
    So, this patch could be dropped depending of the state of the other one.
    
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>
    
    Conflicts:
    	arch/arm/kernel/topology.c

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -258,8 +258,6 @@ void store_cpu_topology(unsigned int cpuid)
 
 	update_siblings_masks(cpuid);
 
-	update_cpu_power(cpuid);
-
 	printk(KERN_INFO "CPU%u: thread %d, cpu %d, socket %d, mpidr %x\n",
 		cpuid, cpu_topology[cpuid].thread_id,
 		cpu_topology[cpuid].core_id,
