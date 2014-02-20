859b7a1d26d7e7e919a41bf8e85d4291ac3f338f
Ethan Chen <intervigil@gmail.com>
2014-02-20 12:46:40 -0800

    Revert "Revert "msm: camera: Extend LED current for Dual LED support.""
    This reverts commit bae631b2d510ce73898e7839105c6248cadc7bd6.

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -644,7 +644,8 @@ enum msm_camera_led_config_t {
 
 struct msm_camera_led_cfg_t {
 	enum msm_camera_led_config_t cfgtype;
-	uint32_t led_current;
+	uint32_t torch_current;
+	uint32_t flash_current[2];
 };
 
 /* sensor init structures and enums */
