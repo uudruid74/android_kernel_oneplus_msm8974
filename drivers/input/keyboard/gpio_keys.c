c64cc8b5dcef07af0dda2f816b676d54ed344dbe
arter97 <qkrwngud825@gmail.com>
2014-03-06 18:04:32 +0900

    mdnie: toggling mdnie negative effect with home button (by gokhanmoral)
    Ported from Galaxy S3 kernel

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -495,12 +495,42 @@ static int gpio_key_init_dvfs(struct gpio_button_data *bdata)
 }
 #endif
 
+static inline int64_t get_time_inms(void) {
+	int64_t tinms;
+	struct timespec cur_time = current_kernel_time();
+	tinms =  cur_time.tv_sec * MSEC_PER_SEC;
+	tinms += cur_time.tv_nsec / NSEC_PER_MSEC;
+	return tinms;
+}
+
+extern void mdnie_toggle_negative(void);
+
 static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 {
+	static int64_t homekey_lasttime = 0;
+	static int homekey_count = 0;
 	const struct gpio_keys_button *button = bdata->button;
 	struct input_dev *input = bdata->input;
 	unsigned int type = button->type ?: EV_KEY;
 	int state = (gpio_get_value_cansleep(button->gpio) ? 1 : 0) ^ button->active_low;
+
+	//mdnie negative effect toggle by gm
+	if (button->code == 172) {
+		if (state) {
+			if (get_time_inms() - homekey_lasttime < 300) {
+				homekey_count++;
+				printk(KERN_INFO "repeated home_key action %d.\n", homekey_count);
+			} else {
+				homekey_count = 0;
+			}
+		} else {
+			if (homekey_count == 3) {
+				mdnie_toggle_negative();
+				homekey_count = 0;
+			}
+			homekey_lasttime = get_time_inms();
+		}
+	}
 
 	printk(KERN_INFO "%s: %s key is %s\n",
 		__func__, button->desc, state ? "pressed" : "released");
