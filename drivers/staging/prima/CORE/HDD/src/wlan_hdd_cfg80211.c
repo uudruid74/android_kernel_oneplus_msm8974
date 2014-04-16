fa6af5beed5117665f72af7dcf9ff5c650a701da
Steve Kondik <shade@chemlab.org>
2014-04-16 15:10:44 -0700

    Enable TDLS
    Change-Id: I58fad507adf2f1ef60ac80c963e573d629d9d69f

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -12958,7 +12958,6 @@ static int __wlan_hdd_cfg80211_add_station(struct wiphy *wiphy,
     hdd_adapter_t *pAdapter =  WLAN_HDD_GET_PRIV_PTR(dev);
     int status = -EPERM;
 #ifdef FEATURE_WLAN_TDLS
-    hdd_adapter_t *pAdapter =  WLAN_HDD_GET_PRIV_PTR(dev);
     u32 mask, set;
     ENTER();
 
