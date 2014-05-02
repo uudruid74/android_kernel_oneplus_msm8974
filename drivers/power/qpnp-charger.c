427b02df1bc81023831277769604e600bcf1c41a
Ethan Chen <intervigil@gmail.com>
2014-05-02 16:32:25 -0700

    power: qpnp-charger: Don't report fatal error for optional attrs
    Change-Id: Ida291b371d94cab80ba0c98bc4abe76bbf22759c

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -6502,7 +6502,7 @@ do {									\
 					"qcom," qpnp_dt_property,	\
 					&chip->prop);			\
 									\
-	if ((retval == -EINVAL) && optional)				\
+	if (retval && optional)						\
 		retval = 0;						\
 	else if (retval)						\
 		pr_err("Error reading " #qpnp_dt_property		\
