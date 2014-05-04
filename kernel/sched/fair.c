00d4e6422011f23c3d9d5acc889723af74aebe62
anarkia1976 <stefano.villa1976@gmail.com>
2014-05-04 13:57:49 +0200

    sched: disable by default GENTLE_FAIR_SLEEPERS
    Conflicts:
    	kernel/ksysfs.c
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1054,6 +1054,13 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 0;
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
