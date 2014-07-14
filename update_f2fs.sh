a501d5b7b70ad58eab0a4e185365ef095b9116c3
arter97 <qkrwngud825@gmail.com>
2014-07-14 20:22:42 +0900

    update_f2fs.sh updated

--- update_f2fs.sh
+++ update_f2fs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 git fetch f2fs
 
-find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs' | while read f2fs; do rm -rf $f2fs; git checkout f2fs/dev -- $f2fs; done
+find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs\|recovery' | while read f2fs; do rm -rf $f2fs; git checkout f2fs/dev -- $f2fs; done
 
 rm -rf fs/f2fs
 git checkout f2fs/dev -- fs/f2fs
 
 git log --oneline f2fs/linux-3.4 | head -1 | awk '{print $1}' | while read patch; do git cherry-pick --no-commit $patch; done
+git cherry-pick --no-commit 7c0f01651f1829ddaeca53252e613c1012c163dd
 
 git status
