86433c9ec8e286bc0b1a93425cedb3f4f0a80dcf
arter97 <qkrwngud825@gmail.com>
2014-07-26 14:49:07 +0900

    ramdisk: default governor to intelliactive

--- ramdisk/sbin/after-boot.sh
+++ ramdisk/sbin/after-boot.sh
@@ -14,3 +14,8 @@ value=$(cat /sys/devices/virtual/lcd/panel/panel/auto_brightness)
 echo "$value" > /sys/devices/virtual/lcd/panel/panel/auto_brightness
 
 echo "1" > /sys/module/intelli_plug/parameters/intelli_plug_active
+
+echo "intelliactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "intelliactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "intelliactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "intelliactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
