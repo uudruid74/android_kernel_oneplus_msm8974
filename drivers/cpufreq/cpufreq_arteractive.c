50f3c2d2dd1b5d193fdd20c3fef00efcf592f985
Junjie Wu <junjiew@codeaurora.org>
2014-08-15 16:20:54 -0700

    cpufreq: interactive: Use del_timer/add_timer_on to rearm timers
    Replace mod_timer_pinned() with del_timer(), add_timer_on().
    mod_timer_pinned() always adds timer onto current CPU. Interactive
    governor expects each CPU's timers to be running on the same CPU.
    If cpufreq_interactive_timer_resched() is called from another CPU,
    the timer will be armed on the wrong CPU.
    
    Replacing mod_timer_pinned() with del_timer() and add_timer_on()
    guarantees timers are still run on the right CPU even if another
    CPU reschedules the timer. This would provide more flexibility
    for future changes.
    
    Change-Id: I3a10be37632afc0ea4e0cc9c86323b9783b216b1
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_arteractive.c

--- drivers/cpufreq/cpufreq_arteractive.c
+++ drivers/cpufreq/cpufreq_arteractive.c
@@ -309,9 +309,9 @@ static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
 	return idle_time;
 }
 
-static void cpufreq_interactive_timer_resched(
-	struct cpufreq_interactive_cpuinfo *pcpu)
+static void cpufreq_interactive_timer_resched(unsigned long cpu)
 {
+	struct cpufreq_interactive_cpuinfo *pcpu = &per_cpu(cpuinfo, cpu);
 	unsigned long expires;
 	unsigned long flags;
 
@@ -330,11 +330,15 @@ static void cpufreq_interactive_timer_resched(
 	       timer_set_deferrable(&pcpu->cpu_timer);
 #endif
 
-	mod_timer_pinned(&pcpu->cpu_timer, expires);
+	del_timer(&pcpu->cpu_timer);
+	pcpu->cpu_timer.expires = expires;
+	add_timer_on(&pcpu->cpu_timer, cpu);
 
 	if (timer_slack_val >= 0 && pcpu->target_freq > pcpu->policy->min) {
 		expires += usecs_to_jiffies(timer_slack_val);
-		mod_timer_pinned(&pcpu->cpu_slack_timer, expires);
+		del_timer(&pcpu->cpu_slack_timer);
+		pcpu->cpu_slack_timer.expires = expires;
+		add_timer_on(&pcpu->cpu_slack_timer, cpu);
 	}
 
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -350,6 +354,7 @@ static void cpufreq_interactive_timer_start(int cpu)
 	unsigned long expires = jiffies + usecs_to_jiffies(timer_rate);
 	unsigned long flags;
 
+	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->cpu_timer.expires = expires;
 	del_timer_sync(&pcpu->cpu_timer);
 	add_timer_on(&pcpu->cpu_timer, cpu);
@@ -360,7 +365,6 @@ static void cpufreq_interactive_timer_start(int cpu)
 		add_timer_on(&pcpu->cpu_slack_timer, cpu);
 	}
 
-	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
 		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
@@ -849,7 +853,7 @@ rearm_if_notmax:
 
 rearm:
 	if (!timer_pending(&pcpu->cpu_timer))
-		cpufreq_interactive_timer_resched(pcpu);
+		cpufreq_interactive_timer_resched(data);
 
 exit:
 	up_read(&pcpu->enable_sem);
@@ -882,7 +886,7 @@ static void cpufreq_interactive_idle_start(void)
 		 * the CPUFreq driver.
 		 */
 		if (!pending) {
-			cpufreq_interactive_timer_resched(pcpu);
+			cpufreq_interactive_timer_resched(smp_processor_id());
 
 			now = ktime_to_us(ktime_get());
 			if ((pcpu->policy->cur == pcpu->policy->max) &&
@@ -911,7 +915,7 @@ static void cpufreq_interactive_idle_end(void)
 
 	/* Arm the timer for 1-2 ticks later if not already. */
 	if (!timer_pending(&pcpu->cpu_timer)) {
-		cpufreq_interactive_timer_resched(pcpu);
+		cpufreq_interactive_timer_resched(smp_processor_id());
 	} else if (time_after_eq(jiffies, pcpu->cpu_timer.expires)) {
 		del_timer(&pcpu->cpu_timer);
 		del_timer(&pcpu->cpu_slack_timer);
@@ -1991,7 +1995,7 @@ static int cpufreq_governor_arteractive(struct cpufreq_policy *policy,
 				down_write(&pcpu->enable_sem);
 				del_timer_sync(&pcpu->cpu_timer);
 				del_timer_sync(&pcpu->cpu_slack_timer);
-				cpufreq_interactive_timer_start(j);
+				cpufreq_interactive_timer_resched(j);
 				pcpu->minfreq_boost = 1;
 				up_write(&pcpu->enable_sem);
 			}
