10f57e462e13231d0b380693a692a9c1c37658f0
arter97 <qkrwngud825@gmail.com>
2014-09-06 11:13:25 +0900

    ramdisk: add dexopt to the waiting list

--- ramdisk/sbin/boot.sh
+++ ramdisk/sbin/boot.sh
@@ -23,6 +23,12 @@ while pgrep bootanimation ; do
 	sleep 1
 done
 
+sleep 1
+
+while pgrep dexopt ; do
+	sleep 1
+done
+
 value=$(cat /sys/devices/virtual/lcd/panel/panel/auto_brightness)
 echo "$value" > /sys/devices/virtual/lcd/panel/panel/auto_brightness
 
