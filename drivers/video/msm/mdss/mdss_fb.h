2824d4e39ddd3f191d62edd85ccd188b3d923b88
Naseer Ahmed <naseer@codeaurora.org>
2014-09-25 15:20:34 -0400

    msm: mdss: Adjust fence timeouts
    Change the final fence wait timeout to be 7s for a total of 10s
    Change the display operation timeout to be 1s more than that
    instead of the excessively large timeout.
    
    Change-Id: If9ae04f9e5993d754b6ccbfdd9e80ec46960e73b
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -39,11 +39,13 @@
 #define WAIT_FENCE_FINAL_TIMEOUT (1 * MSEC_PER_SEC)
 #else
 #define WAIT_FENCE_FIRST_TIMEOUT (3 * MSEC_PER_SEC)
-#define WAIT_FENCE_FINAL_TIMEOUT (10 * MSEC_PER_SEC)
+#define WAIT_FENCE_FINAL_TIMEOUT (7 * MSEC_PER_SEC)
 #endif
-/* Display op timeout should be greater than total timeout */
-#define WAIT_DISP_OP_TIMEOUT ((WAIT_FENCE_FIRST_TIMEOUT + \
-		WAIT_FENCE_FINAL_TIMEOUT) * MDP_MAX_FENCE_FD)
+/* Display op timeout should be greater than the total timeout but not
+ * unreasonably large. Set to 1s more than first wait + final wait which
+ * are already quite long and proceed without any further waits. */
+#define WAIT_DISP_OP_TIMEOUT (WAIT_FENCE_FIRST_TIMEOUT + \
+		WAIT_FENCE_FINAL_TIMEOUT + 1)
 
 #ifndef MAX
 #define  MAX(x, y) (((x) > (y)) ? (x) : (y))
