db68f3158970799ff555026763cf09c122070cb8
arter97 <qkrwngud825@gmail.com>
2014-09-26 16:04:43 +0900

    build_kernel: build dtbtool and mkbootimg with -O2

--- build_kernel_e330k.sh
+++ build_kernel_e330k.sh
@@ -59,9 +59,9 @@ ls -lh $RAMFS_TMP.cpio.lz4
 cd $KERNELDIR
 
 echo "Making new boot image"
-gcc -w -s -pipe -o tools/dtbtool/dtbtool tools/dtbtool/dtbtool.c
+gcc -w -s -pipe -O2 -o tools/dtbtool/dtbtool tools/dtbtool/dtbtool.c
 tools/dtbtool/dtbtool -s 2048 -o arch/arm/boot/dt.img -p scripts/dtc/ arch/arm/boot/
-gcc -w -s -pipe -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
+gcc -w -s -pipe -O2 -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
 tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --dt $KERNELDIR/arch/arm/boot/dt.img --ramdisk $RAMFS_TMP.cpio.lz4 --cmdline 'console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3' --base 0x00000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --second_offset 0x00f00000 -o $KERNELDIR/e330k.img
 if [ "${1}" = "CC=\$(CROSS_COMPILE)gcc" ] ; then
 	dd if=/dev/zero bs=$((20971520-$(stat -c %s e330k.img))) count=1 >> e330k.img
