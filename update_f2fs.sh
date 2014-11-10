3d9736fd968f972ef868488755ed89cf95d22cc2
arter97 <qkrwngud825@gmail.com>
2014-11-10 20:17:50 +0900

    Update update_f2fs.sh script
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- update_f2fs.sh
+++ update_f2fs.sh
@@ -8,5 +8,6 @@ git checkout f2fs/dev -- fs/f2fs
 
 git log --oneline f2fs/linux-3.4 | head -1 | awk '{print $1}' | while read patch; do git cherry-pick --no-commit $patch; done
 git cherry-pick --no-commit c37e7c3b70dba1344c241c5af1697c66b9d37332
+git cherry-pick --no-commit f0d700429e936040f9968a6fd3c089a42d2e4c5f
 
 git status
