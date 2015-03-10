6dd2fbcd09283582defc81aa1d04dbc261bedfae
arter97 <qkrwngud825@gmail.com>
2015-03-11 00:08:41 +0900

    treewide: fix build
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_trace.h
+++ drivers/video/msm/mdss/mdss_mdp_trace.h
@@ -20,6 +20,7 @@
 #define TRACE_INCLUDE_PATH .
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_FILE mdss_mdp_trace
+#include <trace/define_trace.h>
 
 #include <linux/tracepoint.h>
 #include "mdss_mdp.h"
@@ -250,6 +251,3 @@ TRACE_EVENT(mdp_trace_counter,
 );
 
 #endif /* if !defined(TRACE_MDSS_MDP_H) || defined(TRACE_HEADER_MULTI_READ) */
-
-/* This part must be outside protection */
-#include <trace/define_trace.h>
