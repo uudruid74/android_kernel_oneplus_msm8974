7cbc6ebb5d01321e71eb4350e53a28d215623dcb
arter97 <qkrwngud825@gmail.com>
2015-03-11 21:21:40 +0900

    ramdisk: apply intellithermal v2 changes
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/init.lpm.sh
+++ ramdisk/init.lpm.sh
@@ -3,8 +3,8 @@
 export PATH=/res/asset:$PATH
 
 echo "0" > /sys/module/intelli_plug/parameters/intelli_plug_active
-echo "0" > /sys/module/msm_thermal/parameters/enabled
-echo "0" > /sys/module/msm_thermal/core_control/enabled
+echo "0" > /sys/module/msm_thermal_v2/parameters/enabled
+echo "0" > /sys/module/msm_thermal_v2/core_control/enabled
 echo "0" > /sys/devices/system/cpu/cpu1/online
 echo "0" > /sys/devices/system/cpu/cpu2/online
 echo "0" > /sys/devices/system/cpu/cpu3/online
