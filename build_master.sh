73c2907599600a27dcca1497397b5274525eadf5
arter97 <qkrwngud825@gmail.com>
2015-03-21 01:16:22 +0900

    Merge rest of the components from hltekor_stock
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 if [ "${1}" = "skip" ] ; then
-	rm arter97-kernel-n9005-"$(cat version)".zip 2>/dev/null
+	rm arter97-kernel-hltekor-"$(cat version)".zip 2>/dev/null
 	cp boot.img kernelzip/boot.img
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-n9005-"$(cat ../version)"-tmp.zip *
-	zipalign -v 4 arter97-kernel-n9005-"$(cat ../version)"-tmp.zip ../arter97-kernel-n9005-"$(cat ../version)".zip
-	rm arter97-kernel-n9005-"$(cat ../version)"-tmp.zip
+	7z a -mx9 arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip *
+	zipalign -v 4 arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip ../arter97-kernel-hltekor-"$(cat ../version)".zip
+	rm arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip
 	cd ..
-	ls -al arter97-kernel-n9005-"$(cat version)".zip
+	ls -al arter97-kernel-hltekor-"$(cat version)".zip
 	exit 0
 fi
 
 ./build_clean.sh
-./build_kernel_n9005.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
+./build_kernel_hltekor.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
 
-rm arter97-kernel-n9005-"$(cat version)".zip 2>/dev/null
+rm arter97-kernel-hltekor-"$(cat version)".zip 2>/dev/null
 cp boot.img kernelzip/boot.img
 cd kernelzip/
-7z a -mx9 arter97-kernel-n9005-"$(cat ../version)"-tmp.zip *
-zipalign -v 4 arter97-kernel-n9005-"$(cat ../version)"-tmp.zip ../arter97-kernel-n9005-"$(cat ../version)".zip
-rm arter97-kernel-n9005-"$(cat ../version)"-tmp.zip
+7z a -mx9 arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip *
+zipalign -v 4 arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip ../arter97-kernel-hltekor-"$(cat ../version)".zip
+rm arter97-kernel-hltekor-"$(cat ../version)"-tmp.zip
 cd ..
-ls -al arter97-kernel-n9005-"$(cat version)".zip
-
-./build_clean.sh nozip
-./build_kernel_n900t.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
-
-rm arter97-kernel-n900t-"$(cat version)".zip 2>/dev/null
-cp boot.img kernelzip/boot.img
-cd kernelzip/
-7z a -mx9 arter97-kernel-n900t-"$(cat ../version)"-tmp.zip *
-zipalign -v 4 arter97-kernel-n900t-"$(cat ../version)"-tmp.zip ../arter97-kernel-n900t-"$(cat ../version)".zip
-rm arter97-kernel-n900t-"$(cat ../version)"-tmp.zip
-cd ..
-ls -al arter97-kernel-n900t-"$(cat version)".zip
+ls -al arter97-kernel-hltekor-"$(cat version)".zip
