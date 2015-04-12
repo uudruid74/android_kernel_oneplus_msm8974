5c40c83f42b47324c5f35bbd55ed589b89fddcfc
arter97 <qkrwngud825@gmail.com>
2015-04-12 13:04:51 +0900

    Revert "Remove recovery build"
    This reverts commit d34b440e86615804db687e72565b9e47026f6488.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 if [ "${1}" = "skip" ] ; then
 	device=$(echo $(\ls *.img) | sed s/.img//g)
-	rm arter97-kernel-"$device"-"$(cat version)".zip 2>/dev/null
-	cp *.img kernelzip/boot.img
-	cd kernelzip/
-	7z a -mx9 arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip *
-	zipalign -v 4 arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$device"-"$(cat ../version)".zip
-	rm arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip
-	cd ..
-	ls -al arter97-kernel-"$device"-"$(cat version)".zip
+	if [ "$device" = "recovery" ] ; then
+		rm arter97-recovery-i9506-"$(cat version_recovery | awk '{print $1}')".zip 2>/dev/null
+		cp recovery.img recoveryzip/recovery.img
+		cd recoveryzip/
+		cp ../kernelzip/META-INF/com/google/android/update-binary META-INF/com/google/android/update-binary
+		sed -i -e s/PHILZ_VERSION/$(cat ../version_recovery | awk '{print $1}')/g -e s/CWM_VERSION/$(cat ../version_recovery | awk '{print $2 }')/g META-INF/com/google/android/updater-script
+		7z a -mx9 arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip *
+		zipalign -v 4 arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip ../arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')".zip
+		sed -i -e s/$(cat ../version_recovery | awk '{print $1}')/PHILZ_VERSION/g -e s/$(cat ../version_recovery | awk '{print $2 }')/CWM_VERSION/g META-INF/com/google/android/updater-script
+		rm arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip
+		cd ..
+		rm recoveryzip/META-INF/com/google/android/update-binary
+		ls -al arter97-recovery-i9506-"$(cat version_recovery | awk '{print $1}')".zip
+	else
+		rm arter97-kernel-"$device"-"$(cat version)".zip 2>/dev/null
+		cp *.img kernelzip/boot.img
+		cd kernelzip/
+		7z a -mx9 arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip *
+		zipalign -v 4 arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$device"-"$(cat ../version)".zip
+		rm arter97-kernel-"$device"-"$(cat ../version)"-tmp.zip
+		cd ..
+		ls -al arter97-kernel-"$device"-"$(cat version)".zip
+	fi
 	exit 0
 fi
 
@@ -49,3 +64,19 @@ zipalign -v 4 arter97-kernel-i9506-"$(cat ../version)"-tmp.zip ../arter97-kernel
 rm arter97-kernel-i9506-"$(cat ../version)"-tmp.zip
 cd ..
 ls -al arter97-kernel*.zip
+
+./build_clean.sh nozip
+./build_recovery.sh CC='$(CROSS_COMPILE)gcc'
+
+rm arter97-recovery-i9506-"$(cat version_recovery | awk '{print $1}')".zip 2>/dev/null
+cp recovery.img recoveryzip/recovery.img
+cd recoveryzip/
+cp ../kernelzip/META-INF/com/google/android/update-binary META-INF/com/google/android/update-binary
+sed -i -e s/PHILZ_VERSION/$(cat ../version_recovery | awk '{print $1}')/g -e s/CWM_VERSION/$(cat ../version_recovery | awk '{print $2 }')/g META-INF/com/google/android/updater-script
+7z a -mx9 arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip *
+zipalign -v 4 arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip ../arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')".zip
+sed -i -e s/$(cat ../version_recovery | awk '{print $1}')/PHILZ_VERSION/g -e s/$(cat ../version_recovery | awk '{print $2 }')/CWM_VERSION/g META-INF/com/google/android/updater-script
+rm arter97-recovery-i9506-"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip
+cd ..
+rm recoveryzip/META-INF/com/google/android/update-binary
+ls -al arter97-recovery-i9506-"$(cat version_recovery | awk '{print $1}')".zip
