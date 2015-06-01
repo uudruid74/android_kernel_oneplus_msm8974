5852e0ca4c979c8700b62879ee8cbf45b7a2b23f
arter97 <qkrwngud825@gmail.com>
2015-06-02 01:33:35 +0900

    Revert "Revert "kgsl: remove earlysuspend""
    This reverts commit 4d520899741213c828fc0c18537046c763e4807d.
    
    What was I thinking, kgsl already uses runtime_pm!
    
    https://plus.google.com/111524780435806926688/posts/RCV8EP3hFEm
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -139,13 +139,6 @@ static struct adreno_device device_3d0 = {
 		.mem_log = KGSL_LOG_LEVEL_DEFAULT,
 		.pwr_log = KGSL_LOG_LEVEL_DEFAULT,
 		.pm_dump_enable = 0,
-#ifdef CONFIG_HAS_EARLYSUSPEND
-		.display_off = {
-			.level = EARLY_SUSPEND_LEVEL_STOP_DRAWING,
-			.suspend = kgsl_early_suspend_driver,
-			.resume = kgsl_late_resume_driver,
-		},
-#endif
 	},
 	.gmem_base = 0,
 	.gmem_size = SZ_256K,
