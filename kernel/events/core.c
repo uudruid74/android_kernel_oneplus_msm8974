1dad39d836fa1d0661add2db3a0ef32f8bc37f04
Steve Kondik <steve@cyngn.com>
2015-11-16 18:53:48 -0800

    perf: Fix more fallout from "perf: Fix race in removing an event"
    Change-Id: I91f6ad4aa9da020ab23e9ffd4a2d2a0e6c696654

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1222,9 +1222,10 @@ static int __perf_remove_from_context(void *info)
 }
 
 #ifdef CONFIG_SMP
-static void perf_retry_remove(struct perf_event *event)
+static void perf_retry_remove(struct remove_event *rep)
 {
 	int up_ret;
+	struct perf_event *event = rep->event;
 	/*
 	 * CPU was offline. Bring it online so we can
 	 * gracefully exit a perf context.
@@ -1232,14 +1233,13 @@ static void perf_retry_remove(struct perf_event *event)
 	up_ret = cpu_up(event->cpu);
 	if (!up_ret)
 		/* Try the remove call once again. */
-		cpu_function_call(event->cpu, __perf_remove_from_context,
-				  event);
+		cpu_function_call(event->cpu, __perf_remove_from_context, rep);
 	else
 		pr_err("Failed to bring up CPU: %d, ret: %d\n",
 		       event->cpu, up_ret);
 }
 #else
-static void perf_retry_remove(struct perf_event *event)
+static void perf_retry_remove(struct remove_event *rep)
 {
 }
 #endif
@@ -1272,7 +1272,7 @@ static void __ref perf_remove_from_context(struct perf_event *event)
 		ret = cpu_function_call(event->cpu, __perf_remove_from_context,
 					event);
 		if (ret == -ENXIO)
-			perf_retry_remove(event);
+			perf_retry_remove(&re);
 		return;
 	}
 
